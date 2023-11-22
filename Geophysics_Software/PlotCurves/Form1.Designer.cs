9a18f82e67ab09203731d6aa83db230d879d5110
Lai lipeng <100109981+Lipeng-Lai@users.noreply.github.com>
2023-11-22 11:34:17 +0800

    实验一:电测深曲线绘制

---
+++ Geophysics_Software/PlotCurves/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace PlotCurves
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.读入数据ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.读入数据ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 读入数据ToolStripMenuItem
+            // 
+            this.读入数据ToolStripMenuItem.Name = "读入数据ToolStripMenuItem";
+            this.读入数据ToolStripMenuItem.Size = new System.Drawing.Size(98, 28);
+            this.读入数据ToolStripMenuItem.Text = "读入数据";
+            this.读入数据ToolStripMenuItem.Click += new System.EventHandler(this.读入数据ToolStripMenuItem_Click);
+            this.读入数据ToolStripMenuItem.Paint += new System.Windows.Forms.PaintEventHandler(this.读入数据ToolStripMenuItem_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 读入数据ToolStripMenuItem;
+    }
+}
+
---
+++ Geophysics_Software/PlotCurves/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlotCurves
+{
+    public partial class Form1 : Form
+    {
+        List<Curve> curves = new List<Curve>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 读入数据ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World");
+            Curve a = new Curve();
+            a.Add(0.5f, 10);
+            a.Add(1, 18);
+            a.Add(2, 25);
+            a.Add(4, 30);
+            a.Add(8, 22);
+            a.Add(15, 16);
+            a.Add(20, 45);
+            a.Add(30, 75);
+            a.Add(60, 100);
+            a.Print();
+            a.GetRange();
+            a.PrintMaxMin();
+            curves.Add(a); // 向列表传
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+            DrawCurves(curves);
+        }
+
+        void DrawCurves(List<Curve> curves) // curves为列表类, Curve为曲线类
+        {
+
+            Graphics g = this.CreateGraphics();
+            Rectangle winRect = new Rectangle(0, 0, this.Width, this.Height);
+            for (int i = 0; i < curves.Count; i++)
+            {
+                Curve a = curves[i];
+                a.Draw(g, winRect);
+            }
+        }
+
+        private void 读入数据ToolStripMenuItem_Paint(object sender, PaintEventArgs e)
+        {
+            DrawCurves(curves);
+        }
+    }
+}
